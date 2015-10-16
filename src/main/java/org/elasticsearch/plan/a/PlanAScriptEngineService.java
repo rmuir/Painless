1639219d5cb414c3b0d96bbcf74519a93c628057
Robert Muir <rmuir@apache.org>
2015-10-16 00:13:12 -0400

    first stab at (slow but simple) script impl

--- src/main/java/org/elasticsearch/plan/a/PlanAScriptEngineService.java
+++ src/main/java/org/elasticsearch/plan/a/PlanAScriptEngineService.java
@@ -19,59 +19,90 @@
 
 package org.elasticsearch.plan.a;
 
+import org.apache.lucene.index.LeafReaderContext;
+import org.elasticsearch.SpecialPermission;
 import org.elasticsearch.common.component.AbstractComponent;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.script.CompiledScript;
 import org.elasticsearch.script.ExecutableScript;
+import org.elasticsearch.script.LeafSearchScript;
 import org.elasticsearch.script.ScriptEngineService;
 import org.elasticsearch.script.SearchScript;
 import org.elasticsearch.search.lookup.SearchLookup;
 
 import java.io.IOException;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.Map;
 
 public class PlanAScriptEngineService extends AbstractComponent implements ScriptEngineService {
+
+    public static final String NAME = "plan-a";
 
     @Inject
     public PlanAScriptEngineService(Settings settings) {
         super(settings);
     }
-    
-    @Override
-    public void close() throws IOException {}
 
     @Override
     public String[] types() {
-        return null;
+        return new String[] { NAME };
     }
 
     @Override
     public String[] extensions() {
-        return null;
+        return new String[] { NAME };
     }
 
     @Override
     public boolean sandboxed() {
-        return false;
+        return true;
     }
 
     @Override
     public Object compile(String script) {
-        return null;
+        // classloader created here
+        SecurityManager sm = System.getSecurityManager();
+        if (sm != null) {
+            sm.checkPermission(new SpecialPermission());
+        }
+        return AccessController.doPrivileged(new PrivilegedAction<Executable>() {
+            @Override
+            public Executable run() {
+                // NOTE: validation is delayed to allow runtime vars, and we don't have access to per index stuff here
+                return Compiler.compile("something", script, getClass().getClassLoader(), null);
+            }
+        });
     }
 
     @Override
     public ExecutableScript executable(CompiledScript compiledScript, Map<String,Object> vars) {
-        return null;
+        return new ScriptImpl((Executable) compiledScript.compiled(), vars, null);
     }
 
     @Override
     public SearchScript search(CompiledScript compiledScript, SearchLookup lookup, Map<String,Object> vars) {
-        return null;
+        return new SearchScript() {
+            @Override
+            public LeafSearchScript getLeafSearchScript(LeafReaderContext context) throws IOException {
+                return new ScriptImpl((Executable) compiledScript.compiled(), vars, lookup.getLeafSearchLookup(context));
+            }
+
+            @Override
+            public boolean needsScores() {
+                return true; // TODO: maybe even do these different and more like expressions.
+            }
+        };
     }
 
     @Override
-    public void scriptRemoved(CompiledScript script) {}
-    
+    public void scriptRemoved(CompiledScript script) {
+        // nothing to do
+    }
+
+    @Override
+    public void close() throws IOException {
+        // nothing to do
+    }
 }
