0b0922d47d94895185aa9cf82e1df88847056307
Robert Muir <rmuir@apache.org>
2015-10-21 11:58:29 -0400

    reorganize some more and add disabled test for #7

--- src/test/java/org/elasticsearch/plan/a/BinaryOperatorTests.java
+++ src/test/java/org/elasticsearch/plan/a/BinaryOperatorTests.java
@@ -25,33 +25,52 @@ package org.elasticsearch.plan.a;
 // TODO: NaN/Inf/overflow/...
 public class BinaryOperatorTests extends ScriptTestCase {
     
+    // TODO: move to per-type tests and test for each type
     public void testBasics() {
-        assertEquals(6, exec("return 2 * 3;"));
+        assertEquals(2.25F / 1.5F, exec("return 2.25F / 1.5F;"));
+        assertEquals(2.25F % 1.5F, exec("return 2.25F % 1.5F;"));
+        assertEquals(2 - 1, exec("return 2 - 1;"));
+        assertEquals(1 << 2, exec("return 1 << 2;"));
+        assertEquals(4 >> 2, exec("return 4 >> 2;"));
+        assertEquals(-1 >>> 29, exec("return -1 >>> 29;"));
+        assertEquals(5 & 3, exec("return 5 & 3;"));
+        assertEquals(5 & 3L, exec("return 5 & 3L;"));
+        assertEquals(5L & 3, exec("return 5L & 3;"));
+        assertEquals(5 | 3, exec("return 5 | 3;"));
+        assertEquals(5L | 3, exec("return 5L | 3;"));
+        assertEquals(5 | 3L, exec("return 5 | 3L;"));
+        assertEquals(9 ^ 3, exec("return 9 ^ 3;"));
+        assertEquals(9L ^ 3, exec("return 9L ^ 3;"));
+        assertEquals(9 ^ 3L, exec("return 9 ^ 3L;"));
+    }
+    
+    @AwaitsFix(bugUrl = "https://github.com/elastic/Painless/issues/7")
+    public void testLongShifts() {
+        assertEquals(1L << 2, exec("return 1L << 2;"));
+        assertEquals(1 << 2L, exec("return 1 << 2L;"));
+        assertEquals(4 >> 2L, exec("return 4 >> 2L;"));
+        assertEquals(4L >> 2, exec("return 4L >> 2;"));
+        assertEquals(-1L >>> 29, exec("return -1L >>> 29;"));
+        assertEquals(-1 >>> 29L, exec("return -1 >>> 29L;"));
+    }
+    
+    public void testMixedTypes() {
         assertEquals(8, exec("int x = 4; char y = 2; return x*y;"));
-        assertEquals(1.5F, exec("return 2.25F / 1.5F;"));
         assertEquals(0.5, exec("double x = 1; float y = 2; return x / y;"));
-        assertEquals(0.75F, exec("return 2.25F % 1.5F;"));
         assertEquals(1, exec("int x = 3; int y = 2; return x % y;"));
-        assertEquals(3, exec("return 1 + 2;"));
         assertEquals(3.0, exec("double x = 1; byte y = 2; return x + y;"));
-        assertEquals(1, exec("return 2 - 1;"));
         assertEquals(-1, exec("int x = 1; char y = 2; return x - y;"));
-        assertEquals(4, exec("return 1 << 2;"));
         assertEquals(4, exec("int x = 1; char y = 2; return x << y;"));
-        assertEquals(1, exec("return 4 >> 2;"));
         assertEquals(-1, exec("int x = -1; char y = 29; return x >> y;"));
-        assertEquals(7, exec("return -1 >>> 29;"));
         assertEquals(3, exec("int x = -1; char y = 30; return x >>> y;"));
-        assertEquals(1L, exec("return 5L & 3;"));
         assertEquals(1L, exec("int x = 5; long y = 3; return x & y;"));
-        assertEquals(7, exec("return 5 | 3;"));
         assertEquals(7, exec("short x = 5; byte y = 3; return x | y;"));
-        assertEquals(10, exec("return 9 ^ 3;"));
         assertEquals(10, exec("short x = 9; char y = 3; return x ^ y;"));
     }
     
     public void testAddInt() throws Exception {
         assertEquals(1+1, exec("return 1+1;"));
+        assertEquals(1+2, exec("return 1+2;"));
         assertEquals(5+10, exec("return 5+10;"));
         assertEquals(1+1+2, exec("return 1+1+2;"));
         assertEquals((1+1)+2, exec("return (1+1)+2;"));
@@ -63,6 +82,7 @@ public class BinaryOperatorTests extends ScriptTestCase {
     
     public void testAddLong() throws Exception {
         assertEquals(1L+1L, exec("return 1L+1L;"));
+        assertEquals(1L+2L, exec("return 1L+2L;"));
         assertEquals(5L+10L, exec("return 5L+10L;"));
         assertEquals(1L+1L+2L, exec("return 1L+1L+2L;"));
         assertEquals((1L+1L)+2L, exec("return (1L+1L)+2L;"));
@@ -74,6 +94,7 @@ public class BinaryOperatorTests extends ScriptTestCase {
     
     public void testAddFloat() throws Exception {
         assertEquals(1F+1F, exec("return 1F+1F;"));
+        assertEquals(1F+2F, exec("return 1F+2F;"));
         assertEquals(5F+10F, exec("return 5F+10F;"));
         assertEquals(1F+1F+2F, exec("return 1F+1F+2F;"));
         assertEquals((1F+1F)+2F, exec("return (1F+1F)+2F;"));
@@ -85,6 +106,7 @@ public class BinaryOperatorTests extends ScriptTestCase {
     
     public void testAddDouble() throws Exception {
         assertEquals(1.0+1.0, exec("return 1.0+1.0;"));
+        assertEquals(1.0+2.0, exec("return 1.0+2.0;"));
         assertEquals(5.0+10.0, exec("return 5.0+10.0;"));
         assertEquals(1.0+1.0+2.0, exec("return 1.0+1.0+2.0;"));
         assertEquals((1.0+1.0)+2.0, exec("return (1.0+1.0)+2.0;"));
@@ -100,6 +122,7 @@ public class BinaryOperatorTests extends ScriptTestCase {
     
     public void testMultiplyInt() throws Exception {
         assertEquals(1*1, exec("return 1*1;"));
+        assertEquals(2*3, exec("return 2*3;"));
         assertEquals(5*10, exec("return 5*10;"));
         assertEquals(1*1*2, exec("return 1*1*2;"));
         assertEquals((1*1)*2, exec("return (1*1)*2;"));
@@ -110,6 +133,7 @@ public class BinaryOperatorTests extends ScriptTestCase {
     
     public void testMultiplyLong() throws Exception {
         assertEquals(1L*1L, exec("return 1L*1L;"));
+        assertEquals(2L*3L, exec("return 2L*3L;"));
         assertEquals(5L*10L, exec("return 5L*10L;"));
         assertEquals(1L*1L*2L, exec("return 1L*1L*2L;"));
         assertEquals((1L*1L)*2L, exec("return (1L*1L)*2L;"));
@@ -120,6 +144,7 @@ public class BinaryOperatorTests extends ScriptTestCase {
     
     public void testMultiplyFloat() throws Exception {
         assertEquals(1F*1F, exec("return 1F*1F;"));
+        assertEquals(2F*3F, exec("return 2F*3F;"));
         assertEquals(5F*10F, exec("return 5F*10F;"));
         assertEquals(1F*1F*2F, exec("return 1F*1F*2F;"));
         assertEquals((1F*1F)*2F, exec("return (1F*1F)*2F;"));
@@ -130,6 +155,7 @@ public class BinaryOperatorTests extends ScriptTestCase {
     
     public void testMultiplyDouble() throws Exception {
         assertEquals(1.0*1.0, exec("return 1.0*1.0;"));
+        assertEquals(2.0*3.0, exec("return 2.0*3.0;"));
         assertEquals(5.0*10.0, exec("return 5.0*10.0;"));
         assertEquals(1.0*1.0*2.0, exec("return 1.0*1.0*2.0;"));
         assertEquals((1.0*1.0)*2.0, exec("return (1.0*1.0)*2.0;"));
